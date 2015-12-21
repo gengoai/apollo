4f51d797af859234e56356e69d3e3718eac67ebe
David Bracewell <david@my_email.com>
2015-12-21 16:31:43 -0600

    Adding quick create with non-labeled input

--- src/main/java/com/davidbracewell/apollo/ml/sequence/SequenceInput.java
+++ src/main/java/com/davidbracewell/apollo/ml/sequence/SequenceInput.java
@@ -1,7 +1,10 @@
 package com.davidbracewell.apollo.ml.sequence;
 
+import lombok.NonNull;
+
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Optional;
 
 /**
@@ -11,6 +14,14 @@ public class SequenceInput<T> implements Serializable {
   private static final long serialVersionUID = 1L;
   private final ArrayList<T> list = new ArrayList<>();
   private final ArrayList<String> labels = new ArrayList<>();
+
+  public SequenceInput() {
+
+  }
+
+  public SequenceInput(@NonNull Collection<T> collection) {
+    list.addAll(collection);
+  }
 
   public String getLabel(int index) {
     if (index < 0 || index >= labels.size()) {

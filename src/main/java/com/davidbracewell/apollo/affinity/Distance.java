f204d41ea45c7916da533346649a754f99f2888c
David Bracewell <david@my_email.com>
2015-12-23 11:36:10 -0600

    Adding Distance Measures

--- src/main/java/com/davidbracewell/apollo/affinity/Distance.java
+++ src/main/java/com/davidbracewell/apollo/affinity/Distance.java
@@ -48,6 +48,49 @@ public enum Distance implements DistanceMeasure {
       }
       return sum;
     }
+  },
+  EarthMovers {
+    @Override
+    public double calculate(Map<?, ? extends Number> m1, Map<?, ? extends Number> m2) {
+      double last = 0;
+      double sum = 0;
+      for (Object o : Sets.union(m1.keySet(), m2.keySet())) {
+        double d1 = m1.containsKey(o) ? m1.get(o).doubleValue() : 0d;
+        double d2 = m2.containsKey(o) ? m2.get(o).doubleValue() : 0d;
+        double dist = (d1 + last) - d2;
+        sum += Math.abs(dist);
+        last = dist;
+      }
+      return sum;
+    }
+  },
+  Chebyshev {
+    @Override
+    public double calculate(Map<?, ? extends Number> m1, Map<?, ? extends Number> m2) {
+      double max = 0;
+      for (Object o : Sets.union(m1.keySet(), m2.keySet())) {
+        double d1 = m1.containsKey(o) ? m1.get(o).doubleValue() : 0d;
+        double d2 = m2.containsKey(o) ? m2.get(o).doubleValue() : 0d;
+        max = Math.max(max, Math.abs(d1 - d2));
+      }
+      return max;
+    }
+  },
+  KLDivergence {
+    @Override
+    public double calculate(Map<?, ? extends Number> m1, Map<?, ? extends Number> m2) {
+      double kl = 0;
+      for (Object o : Sets.union(m1.keySet(), m2.keySet())) {
+        double d1 = m1.containsKey(o) ? m1.get(o).doubleValue() : 0d;
+        double d2 = m2.containsKey(o) ? m2.get(o).doubleValue() : 0d;
+        if (d1 == 0.0 || d2 == 0.0) {
+          continue;
+        }
+        kl += d1 * Math.log(d1 / d2);
+      }
+      return kl;
+    }
   }
+
 
 }//END OF Distance

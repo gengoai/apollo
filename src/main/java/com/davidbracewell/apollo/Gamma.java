3a29a5b50b5d95fb9b0227ca1044c93650d0e9a8
David Bracewell <david@my_email.com>
2015-12-26 18:46:19 -0600

    Updates

---
+++ src/main/java/com/davidbracewell/apollo/Gamma.java
@@ -0,0 +1,14 @@
+package com.davidbracewell.apollo;
+
+import com.davidbracewell.function.SerializableDoubleUnaryOperator;
+
+/**
+ * @author David B. Bracewell
+ */
+public class Gamma implements SerializableDoubleUnaryOperator {
+  private static final long serialVersionUID = 1L;
+  @Override
+  public double applyAsDouble(double operand) {
+    return 0;
+  }
+}// END OF Gamma
--- src/main/java/com/davidbracewell/apollo/distribution/Binomial.java
+++ src/main/java/com/davidbracewell/apollo/distribution/Binomial.java
@@ -3,36 +3,81 @@ package com.davidbracewell.apollo.distribution;
 import com.davidbracewell.apollo.ApolloMath;
 
 /**
+ * The type Binomial.
+ *
  * @author David B. Bracewell
  */
 public class Binomial implements DiscreteDistribution<Binomial> {
-  private final double probabilityOfSuccess;
   private int nSuccess = 0;
   private int trials = 0;
 
-  public Binomial(double probabilityOfSuccess) {
-    this.probabilityOfSuccess = probabilityOfSuccess;
+  /**
+   * Instantiates a new Binomial.
+   */
+  public Binomial() {
+    this(0, 0);
   }
 
-  public static void main(String[] args) {
-    Binomial binomial = new Binomial(0.5);
-    binomial.increment(1, 100);
-    binomial.increment(0, 100);
-    System.out.println(binomial.probabilityOfSuccess());
-    System.out.println(binomial.probability(100));
+  /**
+   * Instantiates a new Binomial.
+   *
+   * @param numberOfSuccess the number of success
+   * @param numberOfTrials  the number of trials
+   */
+  public Binomial(int numberOfSuccess, int numberOfTrials) {
+    this.nSuccess = numberOfSuccess;
+    this.trials = numberOfTrials;
   }
 
   @Override
   public double probability(int value) {
-    return Math.exp(logProbability(value));
+    double logP = logProbability(value);
+    return Double.isFinite(logP) ? Math.exp(logP) : 0d;
   }
 
+  /**
+   * Gets mean.
+   *
+   * @return the mean
+   */
+  public double getMean() {
+    return nSuccess;
+  }
+
+  /**
+   * Gets variance.
+   *
+   * @return the variance
+   */
+  public double getVariance() {
+    return nSuccess * (1.0 - probabilityOfSuccess());
+  }
+
+
+  /**
+   * Gets number of trials.
+   *
+   * @return the number of trials
+   */
+  public int getNumberOfTrials() {
+    return trials;
+  }
+
+  /**
+   * Probability of success double.
+   *
+   * @return the double
+   */
   public double probabilityOfSuccess() {
-    return probabilityOfSuccess;
+    return (double) nSuccess / (double) trials;
   }
 
   @Override
   public double logProbability(int value) {
+    if (trials <= 0 || value < 0 || value > trials) {
+      return Double.NEGATIVE_INFINITY;
+    }
+    double probabilityOfSuccess = probabilityOfSuccess();
     return ApolloMath.logGamma(trials + 1) -
       ApolloMath.logGamma(value + 1) -
       ApolloMath.logGamma(trials - value + 1) +
@@ -50,6 +95,10 @@ public class Binomial implements DiscreteDistribution<Binomial> {
       nSuccess += value;
     }
     trials += value;
+    if (trials < 0) {
+      trials = 0;
+      nSuccess = 0;
+    }
     return this;
   }
 

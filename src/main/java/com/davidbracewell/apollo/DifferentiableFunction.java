0694e1716f2cffc57c747a0ed2414cfa4f2438e0
David Bracewell <david@my_email.com>
2016-01-07 11:08:18 -0600

    Updates

--- src/main/java/com/davidbracewell/apollo/DifferentiableFunction.java
+++ src/main/java/com/davidbracewell/apollo/DifferentiableFunction.java
@@ -27,10 +27,11 @@ import com.davidbracewell.function.SerializableDoubleUnaryOperator;
  * @author David B. Bracewell
  */
 public interface DifferentiableFunction extends SerializableDoubleUnaryOperator {
-  double H = 0.000001;
+  double H = 1E-5;
+  double TWO_H = 2.0 * H;
 
   default double gradient(double value) {
-    return (applyAsDouble(value + H) - applyAsDouble(value)) / H;
+    return (applyAsDouble(value + H) - applyAsDouble(value - H)) / TWO_H;
   }
 
 }//END OF DifferentiableFunction

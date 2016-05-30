ad1bff0394e319d5420b00cd466996c4156d267d
David Bracewell <david@my_email.com>
2016-05-30 17:18:50 -0500

    Refactoring SequenceFeaturizer to support CacheProxy and to add methods that mirror Featurizer. Potential change of Featurizer -> InstanceFeaturizer to pull out methods only useful for Instances.

--- src/main/java/com/davidbracewell/apollo/ml/sequence/SequenceFeaturizer.java
+++ src/main/java/com/davidbracewell/apollo/ml/sequence/SequenceFeaturizer.java
@@ -3,7 +3,11 @@ package com.davidbracewell.apollo.ml.sequence;
 import com.davidbracewell.apollo.ml.Feature;
 import com.davidbracewell.apollo.ml.Featurizer;
 import com.davidbracewell.apollo.ml.Instance;
+import com.davidbracewell.cache.CacheProxy;
+import com.davidbracewell.cache.Cached;
+import com.davidbracewell.conversion.Cast;
 import com.davidbracewell.function.SerializableFunction;
+import com.davidbracewell.stream.MStream;
 import lombok.NonNull;
 
 import java.util.*;
@@ -16,7 +20,13 @@ import java.util.*;
  */
 public interface SequenceFeaturizer<INPUT> extends Featurizer<ContextualIterator<INPUT>> {
 
-  static Set<Feature> featureSet(Feature... features){
+  /**
+   * Feature set set.
+   *
+   * @param features the features
+   * @return the set
+   */
+  static Set<Feature> featureSet(Feature... features) {
     return new HashSet<>(Arrays.asList(features));
   }
 
@@ -32,10 +42,55 @@ public interface SequenceFeaturizer<INPUT> extends Featurizer<ContextualIterator
       private static final long serialVersionUID = 1L;
 
       @Override
+      @Cached
       public Set<Feature> apply(ContextualIterator<T> tContextualIterator) {
         return new HashSet<>(function.apply(tContextualIterator));
       }
     };
+  }
+
+  @Override
+  default SequenceFeaturizer<INPUT> cache(String cacheName) {
+    return CacheProxy.cache(this, cacheName);
+  }
+
+  @Override
+  default SequenceFeaturizer<INPUT> cache() {
+    return CacheProxy.cache(this);
+  }
+
+
+  /**
+   * Chain sequence featurizer.
+   *
+   * @param <T>         the type parameter
+   * @param featurizers the featurizers
+   * @return the sequence featurizer
+   */
+  @SafeVarargs
+  static <T> SequenceFeaturizer<T> chain(@NonNull SequenceFeaturizer<? super T>... featurizers) {
+    return new SequenceFeaturizer<T>() {
+      private static final long serialVersionUID = 1L;
+      final Set<SequenceFeaturizer<? super T>> extractors = new LinkedHashSet<>(Arrays.asList(featurizers));
+
+      @Override
+      @Cached
+      public Set<Feature> apply(ContextualIterator<T> tContextualIterator) {
+        Set<Feature> features = new HashSet<>();
+        extractors.forEach(ex -> features.addAll(ex.apply(Cast.as(tContextualIterator))));
+        return features;
+      }
+    };
+  }
+
+  /**
+   * Extract sequence m stream.
+   *
+   * @param stream the stream
+   * @return the m stream
+   */
+  default MStream<Sequence> extractSequence(@NonNull MStream<ContextualIterator<INPUT>> stream) {
+    return stream.map(this::extractSequence);
   }
 
   /**

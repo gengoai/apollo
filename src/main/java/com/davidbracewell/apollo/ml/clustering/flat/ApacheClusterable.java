a462ddb4dd47ffeb46f8e9b8d8b248ac3365d2e0
David Bracewell <david@my_email.com>
2016-07-26 13:37:25 -0500

    DBSCAN via Apache

---
+++ src/main/java/com/davidbracewell/apollo/ml/clustering/flat/ApacheClusterable.java
@@ -0,0 +1,30 @@
+package com.davidbracewell.apollo.ml.clustering.flat;
+
+import com.davidbracewell.Lazy;
+import com.davidbracewell.apollo.linalg.LabeledVector;
+import org.apache.commons.math3.ml.clustering.Clusterable;
+
+import java.io.Serializable;
+
+/**
+ * @author David B. Bracewell
+ */
+public class ApacheClusterable implements Clusterable, Serializable {
+  private static final long serialVersionUID = 1L;
+  private volatile LabeledVector vector;
+  private Lazy<double[]> point = new Lazy<>(() -> vector.toArray());
+
+  public ApacheClusterable(LabeledVector vector) {
+    this.vector = vector;
+  }
+
+  public LabeledVector getVector() {
+    return vector;
+  }
+
+  @Override
+  public double[] getPoint() {
+    return new double[0];
+  }
+
+}// END OF ApacheClusterable
---
+++ src/main/java/com/davidbracewell/apollo/ml/clustering/flat/DBSCAN.java
@@ -0,0 +1,57 @@
+package com.davidbracewell.apollo.ml.clustering.flat;
+
+import com.davidbracewell.apollo.affinity.Distance;
+import com.davidbracewell.apollo.affinity.DistanceMeasure;
+import com.davidbracewell.apollo.linalg.LabeledVector;
+import com.davidbracewell.apollo.ml.clustering.Cluster;
+import com.davidbracewell.apollo.ml.clustering.Clusterer;
+import com.davidbracewell.stream.MStream;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import org.apache.commons.math3.ml.clustering.DBSCANClusterer;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author David B. Bracewell
+ */
+public class DBSCAN extends Clusterer<FlatHardClustering> {
+  @Getter
+  @Setter
+  private double eps;
+  @Getter
+  @Setter
+  private int minPts;
+  @Getter
+  @Setter(onParam = @_({@NonNull}))
+  private DistanceMeasure distanceMeasure;
+
+
+  public DBSCAN() {
+    this(Distance.Euclidean, 0.01, 2);
+  }
+
+  public DBSCAN(@NonNull DistanceMeasure distanceMeasure, double eps, int minPts) {
+    this.distanceMeasure = distanceMeasure;
+    this.eps = eps;
+    this.minPts = minPts;
+  }
+
+
+  @Override
+  public FlatHardClustering cluster(MStream<LabeledVector> instances) {
+    DBSCANClusterer<ApacheClusterable> clusterer = new DBSCANClusterer<>(eps, minPts, new ApacheDistanceMeasure(distanceMeasure));
+    List<Cluster> clusters = clusterer.cluster(instances.map(ApacheClusterable::new).collect())
+      .stream()
+      .map(c -> {
+        com.davidbracewell.apollo.ml.clustering.Cluster cp = new com.davidbracewell.apollo.ml.clustering.Cluster();
+        c.getPoints().forEach(ap -> cp.addPoint(ap.getVector()));
+        return cp;
+      }).collect(Collectors.toList());
+    return new FlatHardClustering(getEncoderPair(), distanceMeasure, clusters);
+  }
+
+
+}// END OF DBSCAN

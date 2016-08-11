5c7f5d0b80f1491c1756b395ce08d5c06a71491c
David Bracewell <david@my_email.com>
2016-08-11 15:32:46 -0500

    Adding vector compositions.

--- src/main/java/com/davidbracewell/apollo/linalg/VectorComposition.java
+++ src/main/java/com/davidbracewell/apollo/linalg/VectorComposition.java
@@ -6,15 +6,31 @@ import java.util.Arrays;
 import java.util.Collection;
 
 /**
+ * The interface Vector composition.
+ *
  * @author David B. Bracewell
  */
 @FunctionalInterface
 public interface VectorComposition {
 
+  /**
+   * Compose vector.
+   *
+   * @param dimension the dimension
+   * @param vectors   the vectors
+   * @return the vector
+   */
   default Vector compose(int dimension, @NonNull Vector... vectors) {
     return compose(dimension,Arrays.asList(vectors));
   }
 
+  /**
+   * Compose vector.
+   *
+   * @param dimension the dimension
+   * @param vectors   the vectors
+   * @return the vector
+   */
   Vector compose(int dimension, Collection<Vector> vectors);
 
 }// END OF VectorComposition
--- src/main/java/com/davidbracewell/apollo/linalg/VectorCompositions.java
+++ src/main/java/com/davidbracewell/apollo/linalg/VectorCompositions.java
@@ -2,6 +2,7 @@ package com.davidbracewell.apollo.linalg;
 
 import lombok.NonNull;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 /**
@@ -24,5 +25,66 @@ public enum VectorCompositions implements VectorComposition {
       }
       return rval.mapDivideSelf(count);
     }
+  },
+  Sum {
+    @Override
+    public Vector compose(int dimension, @NonNull Collection<Vector> vectors) {
+      if (vectors.size() == 0) {
+        return new SparseVector(dimension);
+      }
+      Vector rval = new DenseVector(dimension);
+      vectors.forEach(rval::addSelf);
+      return rval;
+    }
+  },
+  PointWiseMultiply {
+    @Override
+    public Vector compose(int dimension, @NonNull Collection<Vector> vectors) {
+      if (vectors.size() == 0) {
+        return new SparseVector(dimension);
+      }
+      Vector rval = new DenseVector(dimension);
+      vectors.stream().filter(vector -> vector.magnitude() > 0).forEach(rval::multiplySelf);
+      return rval;
+    }
+  },
+  Max {
+    @Override
+    public Vector compose(int dimension, @NonNull Collection<Vector> vectors) {
+      if (vectors.size() == 0) {
+        return new SparseVector(dimension);
+      }
+      double[] values = new double[dimension];
+      Arrays.fill(values, Double.NEGATIVE_INFINITY);
+      Vector rval = new DenseVector(values);
+      vectors.stream().filter(vector -> vector.magnitude() > 0)
+             .forEach(v -> v.forEach(e -> {
+                        if (e.getValue() > rval.get(e.getIndex())) {
+                          rval.set(e.getIndex(), e.getValue());
+                        }
+                      })
+             );
+      return rval;
+    }
+  },
+  Min {
+    @Override
+    public Vector compose(int dimension, @NonNull Collection<Vector> vectors) {
+      if (vectors.size() == 0) {
+        return new SparseVector(dimension);
+      }
+      double[] values = new double[dimension];
+      Arrays.fill(values, Double.POSITIVE_INFINITY);
+      Vector rval = new DenseVector(values);
+      vectors.stream().filter(vector -> vector.magnitude() > 0)
+             .forEach(v -> v.forEach(e -> {
+                        if (e.getValue() < rval.get(e.getIndex())) {
+                          rval.set(e.getIndex(), e.getValue());
+                        }
+                      })
+             );
+      return rval;
+    }
   }
+
 }//END OF VectorCompositions

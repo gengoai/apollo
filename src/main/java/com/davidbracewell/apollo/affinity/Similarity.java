e8fbbb468a7f27bb5ce1d2702902ac574484177b
David Bracewell <david@my_email.com>
2016-08-12 09:32:33 -0500

    Adding new similarity.

--- src/main/java/com/davidbracewell/apollo/affinity/Similarity.java
+++ src/main/java/com/davidbracewell/apollo/affinity/Similarity.java
@@ -73,7 +73,40 @@ public enum Similarity implements SimilarityMeasure {
 
     @Override
     public double calculate(@NonNull ContingencyTable table) {
-      Preconditions.checkArgument(table.rowCount() == table.columnCount() && table.rowCount() == 2, "Only supports 2x2 contingency tables.");
+      Preconditions.checkArgument(table.rowCount() == table.columnCount() && table.rowCount() == 2,
+                                  "Only supports 2x2 contingency tables.");
+      return 2 * table.get(0, 0) / (table.columnSum(0) + table.rowSum(0));
+    }
+  },
+  /**
+   * http://wortschatz.uni-leipzig.de/~sbordag/aalw05/Referate/14_MiningRelationen_WSA_opt/Curran_03.pdf
+   */
+  DiceGen2 {
+    @Override
+    public double calculate(@NonNull Map<?, ? extends Number> m1, @NonNull Map<?, ? extends Number> m2) {
+      double dp = 0;
+      double sum = 0;
+
+      Number ov;
+      for (Map.Entry<?, ? extends Number> e : m1.entrySet()) {
+        ov = m2.get(e.getKey());
+        if (ov != null) {
+          dp += e.getValue().doubleValue() * ov.doubleValue();
+        }
+        sum += e.getValue().doubleValue();
+      }
+
+      for (Map.Entry<?, ? extends Number> e : m2.entrySet()) {
+        sum += e.getValue().doubleValue();
+      }
+
+      return sum == 0 ? 0.0 : dp / sum;
+    }
+
+    @Override
+    public double calculate(@NonNull ContingencyTable table) {
+      Preconditions.checkArgument(table.rowCount() == table.columnCount() && table.rowCount() == 2,
+                                  "Only supports 2x2 contingency tables.");
       return 2 * table.get(0, 0) / (table.columnSum(0) + table.rowSum(0));
     }
   },
@@ -102,7 +135,8 @@ public enum Similarity implements SimilarityMeasure {
 
     @Override
     public double calculate(@NonNull ContingencyTable table) {
-      Preconditions.checkArgument(table.rowCount() == table.columnCount() && table.rowCount() == 2, "Only supports 2x2 contingency tables.");
+      Preconditions.checkArgument(table.rowCount() == table.columnCount() && table.rowCount() == 2,
+                                  "Only supports 2x2 contingency tables.");
       return table.get(0, 0) / Math.sqrt(table.columnSum(0) + table.rowSum(0));
     }
   },
@@ -131,7 +165,8 @@ public enum Similarity implements SimilarityMeasure {
 
     @Override
     public double calculate(@NonNull ContingencyTable table) {
-      Preconditions.checkArgument(table.rowCount() == table.columnCount() && table.rowCount() == 2, "Only supports 2x2 contingency tables.");
+      Preconditions.checkArgument(table.rowCount() == table.columnCount() && table.rowCount() == 2,
+                                  "Only supports 2x2 contingency tables.");
       return table.get(0, 0) / (table.get(0, 0) + table.get(0, 1) + table.get(1, 0));
     }
   },
@@ -161,7 +196,8 @@ public enum Similarity implements SimilarityMeasure {
     @Override
     public double calculate(@NonNull ContingencyTable table) {
       Preconditions.checkNotNull(table);
-      Preconditions.checkArgument(table.rowCount() == table.columnCount() && table.rowCount() == 2, "Only supports 2x2 contingency tables.");
+      Preconditions.checkArgument(table.rowCount() == table.columnCount() && table.rowCount() == 2,
+                                  "Only supports 2x2 contingency tables.");
       return table.get(0, 0) / Math.min(table.columnSum(0), table.rowSum(0));
     }
   },
@@ -174,6 +210,27 @@ public enum Similarity implements SimilarityMeasure {
     @Override
     public DistanceMeasure asDistanceMeasure() {
       return Distance.Angular;
+    }
+  },
+  KL_DIVERGENCE {
+    @Override
+    public double calculate(@NonNull Map<?, ? extends Number> m1, @NonNull Map<?, ? extends Number> m2) {
+      double d = 0;
+      Number ov;
+
+      for (Map.Entry<?, ? extends Number> e : m2.entrySet()) {
+        ov = m1.get(e.getKey());
+        if (ov != null && ov.doubleValue() > 0 && e.getValue().doubleValue() > 0) {
+          d += e.getValue().doubleValue() * Math.log(e.getValue().doubleValue() / ov.doubleValue());
+        }
+      }
+
+      return d;
+    }
+
+    @Override
+    public DistanceMeasure asDistanceMeasure() {
+      return new InverseSimilarityDistance(this);
     }
   }
 }//END OF SimilarityMeasures

7ddb354865fcd6f590367e3817cce7d9fde6c7b4
David Bracewell <david@my_email.com>
2016-10-16 19:08:37 -0500

    Default to 0 mean 0 standard deviation for Normal Distribution

--- src/main/java/com/davidbracewell/apollo/distribution/NormalDistribution.java
+++ src/main/java/com/davidbracewell/apollo/distribution/NormalDistribution.java
@@ -8,73 +8,73 @@ import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
  * @author David B. Bracewell
  */
 public class NormalDistribution implements RealDistribution<NormalDistribution> {
-  private static final long serialVersionUID = 1L;
-  private DescriptiveStatistics statistics = new DescriptiveStatistics();
-  private org.apache.commons.math3.distribution.NormalDistribution wrapped = null;
-  private boolean dirty = true;
+   private static final long serialVersionUID = 1L;
+   private DescriptiveStatistics statistics = new DescriptiveStatistics();
+   private org.apache.commons.math3.distribution.NormalDistribution wrapped = null;
+   private boolean dirty = true;
 
-  /**
-   * Instantiates a new Normal distribution.
-   */
-  public NormalDistribution() {
+   /**
+    * Instantiates a new Normal distribution.
+    */
+   public NormalDistribution() {
+      this(0, 0);
+   }
 
-  }
+   /**
+    * Instantiates a new Normal distribution.
+    *
+    * @param mean              the mean
+    * @param standardDeviation the standard deviation
+    */
+   public NormalDistribution(double mean, double standardDeviation) {
+      this.statistics = null;
+      this.wrapped = new org.apache.commons.math3.distribution.NormalDistribution(mean, standardDeviation);
+      this.dirty = false;
+   }
 
-  /**
-   * Instantiates a new Normal distribution.
-   *
-   * @param mean              the mean
-   * @param standardDeviation the standard deviation
-   */
-  public NormalDistribution(double mean, double standardDeviation) {
-    this.statistics = null;
-    this.wrapped = new org.apache.commons.math3.distribution.NormalDistribution(mean, standardDeviation);
-    this.dirty = false;
-  }
+   @Override
+   public double probability(double value) {
+      return get().probability(value);
+   }
 
-  @Override
-  public double probability(double value) {
-    return get().probability(value);
-  }
+   @Override
+   public double cumulativeProbability(double value) {
+      return get().cumulativeProbability(value);
+   }
 
-  @Override
-  public double cumulativeProbability(double value) {
-    return get().cumulativeProbability(value);
-  }
+   @Override
+   public double cumulativeProbability(double lowerBound, double higherBound) {
+      return get().probability(lowerBound, higherBound);
+   }
 
-  @Override
-  public double cumulativeProbability(double lowerBound, double higherBound) {
-    return get().probability(lowerBound, higherBound);
-  }
+   @Override
+   public double inverseCumulativeProbability(double p) {
+      return get().inverseCumulativeProbability(p);
+   }
 
-  @Override
-  public double inverseCumulativeProbability(double p) {
-    return get().inverseCumulativeProbability(p);
-  }
+   @Override
+   public double sample() {
+      return get().sample();
+   }
 
-  @Override
-  public double sample() {
-    return get().sample();
-  }
+   private org.apache.commons.math3.distribution.NormalDistribution get() {
+      if (wrapped == null || dirty) {
+         dirty = false;
+         wrapped = new org.apache.commons.math3.distribution.NormalDistribution(statistics.getMean(),
+                                                                                statistics.getStandardDeviation());
+      }
+      return wrapped;
+   }
 
-  private org.apache.commons.math3.distribution.NormalDistribution get() {
-    if (wrapped == null || dirty) {
+   @Override
+   public NormalDistribution increment(double value) {
+      if (statistics == null) {
+         throw new UnsupportedOperationException("Distribution initialized with a mean and standard deviation");
+      }
       dirty = false;
-      wrapped = new org.apache.commons.math3.distribution.NormalDistribution(statistics.getMean(),
-                                                                             statistics.getStandardDeviation());
-    }
-    return wrapped;
-  }
-
-  @Override
-  public NormalDistribution increment(double value) {
-    if (statistics == null) {
-      throw new UnsupportedOperationException("Distribution initialized with a mean and standard deviation");
-    }
-    dirty = false;
-    statistics.addValue(value);
-    return this;
-  }
+      statistics.addValue(value);
+      return this;
+   }
 
 
 }// END OF NormalDistribution

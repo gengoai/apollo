9df3b5b66bdea817ed31668de9f281ada05c9805
David Bracewell <david@my_email.com>
2017-06-01 10:51:14 -0500

    javadoc and reformatting

--- src/main/java/com/davidbracewell/apollo/analysis/Inequality.java
+++ src/main/java/com/davidbracewell/apollo/analysis/Inequality.java
@@ -6,35 +6,50 @@ package com.davidbracewell.apollo.analysis;
  * @author David B. Bracewell
  */
 public enum Inequality {
-    GREATER_THAN {
-        @Override
-        public boolean compare(double beingCompared, double comparedAgainst) {
-            return beingCompared > comparedAgainst;
-        }
-    }, GREATER_THAN_EQUAL {
-        @Override
-        public boolean compare(double beingCompared, double comparedAgainst) {
-            return beingCompared >= comparedAgainst;
-        }
-    }, LESS_THAN {
-        @Override
-        public boolean compare(double beingCompared, double comparedAgainst) {
-            return beingCompared < comparedAgainst;
-        }
-    }, LESS_THAN_EQUAL {
-        @Override
-        public boolean compare(double beingCompared, double comparedAgainst) {
-            return beingCompared <= comparedAgainst;
-        }
-    };
+   /**
+    * The Greater than.
+    */
+   GREATER_THAN {
+      @Override
+      public boolean compare(double beingCompared, double comparedAgainst) {
+         return beingCompared > comparedAgainst;
+      }
+   },
+   /**
+    * The Greater than equal.
+    */
+   GREATER_THAN_EQUAL {
+      @Override
+      public boolean compare(double beingCompared, double comparedAgainst) {
+         return beingCompared >= comparedAgainst;
+      }
+   },
+   /**
+    * The Less than.
+    */
+   LESS_THAN {
+      @Override
+      public boolean compare(double beingCompared, double comparedAgainst) {
+         return beingCompared < comparedAgainst;
+      }
+   },
+   /**
+    * The Less than equal.
+    */
+   LESS_THAN_EQUAL {
+      @Override
+      public boolean compare(double beingCompared, double comparedAgainst) {
+         return beingCompared <= comparedAgainst;
+      }
+   };
 
-    /**
-     * Determines if the inequality holds for n1 when compared with n2. For example, is n1 > n2?
-     *
-     * @param beingCompared   The number being compared
-     * @param comparedAgainst The number being compared against
-     * @return true if the inequality holds
-     */
-    public abstract boolean compare(double beingCompared, double comparedAgainst);
+   /**
+    * Determines if the inequality holds for n1 when compared with n2. For example, is n1 > n2?
+    *
+    * @param beingCompared   The number being compared
+    * @param comparedAgainst The number being compared against
+    * @return true if the inequality holds
+    */
+   public abstract boolean compare(double beingCompared, double comparedAgainst);
 
 }// END OF Inequality

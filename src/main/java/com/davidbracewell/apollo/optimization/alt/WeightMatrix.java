e842c6fb173145eb5b996aa0e768c6a1955993ea
David Bracewell <david@my_email.com>
2017-07-11 13:49:32 -0500

    updates

--- src/main/java/com/davidbracewell/apollo/optimization/alt/WeightMatrix.java
+++ src/main/java/com/davidbracewell/apollo/optimization/alt/WeightMatrix.java
@@ -5,19 +5,10 @@ import com.davidbracewell.apollo.linalg.SparseMatrix;
 import com.davidbracewell.apollo.linalg.Vector;
 import com.davidbracewell.apollo.optimization.activation.Activation;
 import com.davidbracewell.guava.common.base.Preconditions;
-import com.davidbracewell.guava.common.base.Stopwatch;
 import lombok.Getter;
-import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.factory.Nd4j;
 
 import java.io.Serializable;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
-
-import static com.davidbracewell.tuple.Tuples.$;
-import static org.nd4j.linalg.ops.transforms.Transforms.sigmoid;
 
 /**
  * @author David B. Bracewell
@@ -38,33 +29,10 @@ public class WeightMatrix implements Serializable {
       this.numCols = nC;
    }
 
-   public static void main(String[] args) {
-      INDArray layer1 = Nd4j.rand(256, 100_000);
-      INDArray layer2 = Nd4j.rand(1, 256);
-
-      Stopwatch sw = Stopwatch.createStarted();
-      for (int i = 0; i < 25_000; i++) {
-         INDArray a0 = Nd4j.rand(1, 100_000);
-         INDArray a1 = sigmoid(layer1.mmul(a0.transposei()));
-         INDArray a2 = sigmoid(layer2.mmul(a1));
-
-         INDArray a3Delta = a2.sub(Nd4j.create(1));
-         INDArray a2Delta = a3Delta.mmul(layer2);
-         INDArray a1Delta = a2Delta.mmul(layer1);
-
-         INDArray a2Gradient = a1.mmul(a3Delta);
-         INDArray a1Gradient = a0.transposei().mmul(a2Delta).transposei();
-
-         layer1.subi(a1Gradient);
-         layer2.subi(a2Gradient);
-
-         if (i % 1000 == 0) {
-            System.out.println(i + ": " + sw.elapsed(TimeUnit.SECONDS));
-         }
-
-      }
-
-      System.out.println(sw.elapsed(TimeUnit.SECONDS));
+   public WeightMatrix(WeightVector weightVector) {
+      this.rows = new WeightVector[]{weightVector};
+      this.numRows = 1;
+      this.numCols = weightVector.getWeights().dimension();
    }
 
    public Vector biases() {
@@ -87,22 +55,6 @@ public class WeightMatrix implements Serializable {
 
    public Vector dot(Vector input) {
       return dot(input, Activation.LINEAR);
-   }
-
-   public Iterator<Map.Entry<Integer, WeightVector>> entryIterator() {
-      return new Iterator<Map.Entry<Integer, WeightVector>>() {
-         int row = 0;
-
-         @Override
-         public boolean hasNext() {
-            return row < numRows;
-         }
-
-         @Override
-         public Map.Entry<Integer, WeightVector> next() {
-            return $(row, rows[row++]);
-         }
-      };
    }
 
    public WeightVector get(int r) {

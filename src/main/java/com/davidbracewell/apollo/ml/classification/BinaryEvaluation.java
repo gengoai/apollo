2fe02a256f20ffc657207740295322b8859a1fc4
David Bracewell <david@davidbracewell.com>
2018-03-10 21:43:28 -0600

    update

--- src/main/java/com/davidbracewell/apollo/ml/classification/BinaryEvaluation.java
+++ src/main/java/com/davidbracewell/apollo/ml/classification/BinaryEvaluation.java
@@ -6,12 +6,12 @@ import com.davidbracewell.apollo.ml.data.Dataset;
 import com.davidbracewell.apollo.ml.encoder.LabelEncoder;
 import com.davidbracewell.conversion.Cast;
 import com.davidbracewell.string.TableFormatter;
-import com.davidbracewell.tuple.Tuple2;
+import org.apache.commons.math3.stat.inference.MannWhitneyUTest;
+import org.apache.mahout.math.list.DoubleArrayList;
 
 import java.io.PrintStream;
-import java.util.*;
-
-import static com.davidbracewell.tuple.Tuples.$;
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * @author David B. Bracewell
@@ -19,7 +19,7 @@ import static com.davidbracewell.tuple.Tuples.$;
 public class BinaryEvaluation implements ClassifierEvaluation {
    private static final long serialVersionUID = 1L;
 
-   private final List<Tuple2<Boolean, Double>> results = new ArrayList<>();
+   private final DoubleArrayList[] prob = {new DoubleArrayList(), new DoubleArrayList()};
    private double positive = 0d;
    private double negative = 0d;
    private double tp = 0;
@@ -59,27 +59,25 @@ public class BinaryEvaluation implements ClassifierEvaluation {
 
 
    public void entry(String gold, double[] distribution) {
-      boolean isTrue = gold.equals(positiveLabel);
+      int predictedClass = distribution[1] >= 0.5 ? 1 : 0;
+      int goldClass = gold.equals(positiveLabel) ? 1 : 0;
 
-      results.add($(isTrue, distribution[isTrue ? 1 : 0]));
-
-      if (isTrue) {
+      prob[predictedClass].add(distribution[1]);
+      if (goldClass == 1) {
          positive++;
-         if (distribution[1] > distribution[0]) {
+         if (predictedClass == 1) {
             tp++;
          } else {
             fn++;
          }
       } else {
          negative++;
-         if (distribution[0] > distribution[1]) {
-            tn++;
-         } else {
+         if (predictedClass == 1) {
             fp++;
+         } else {
+            tn++;
          }
-
       }
-
    }
 
 
@@ -105,14 +103,16 @@ public class BinaryEvaluation implements ClassifierEvaluation {
    public void merge(Evaluation<Instance, Classifier> evaluation) {
       if (evaluation instanceof BinaryEvaluation) {
          BinaryEvaluation bce = Cast.as(evaluation);
-         this.results.addAll(bce.results);
+         this.prob[0].addAllOf(bce.prob[0]);
+         this.prob[1].addAllOf(bce.prob[1]);
       } else {
          throw new IllegalArgumentException();
       }
    }
 
    public double auc() {
-//      MannWhitneyUTest mwu = new MannWhitneyUTest();
+      MannWhitneyUTest mwu = new MannWhitneyUTest();
+      return mwu.mannWhitneyU(prob[0].elements(), prob[1].elements());
 //      double[] x = new double[results.size()];
 //      double[] y = new double[results.size()];
 //      for (int i = 0; i < results.size(); i++) {
@@ -120,31 +120,31 @@ public class BinaryEvaluation implements ClassifierEvaluation {
 //         x[i] = results.get(i).v2;
 //      }
 //      double auc = mwu.mannWhitneyU(x, y);
-      results.sort(Comparator.comparing(Tuple2::getV2));
-      double[] rank = new double[results.size()];
-      for (int i = 0; i < results.size(); i++) {
-         double conf = results.get(i).v2;
-
-         if (i + 1 == results.size() || conf != results.get(i + 1).v2) {
-            rank[i] = i + 1;
-         } else {
-            int j = i + 1;
-            for (; j < results.size() && conf == results.get(j).v2; j++) ;
-            double r = (i + 1 + j) / 2.0;
-            for (int k = i; k < j; k++) rank[k] = r;
-            i = j - 1;
-         }
-      }
-
-      double auc = 0;
-      for (int i = 0; i < results.size(); i++) {
-         if (results.get(i).v1) {
-            auc += rank[i];
-         }
-      }
-
-      auc = (auc - (positive * (positive + 1) / 2.0)) / (positive * negative);
-      return auc;
+//      results.sort(Comparator.comparing(Tuple2::getV2));
+//      double[] rank = new double[results.size()];
+//      for (int i = 0; i < results.size(); i++) {
+//         double conf = results.get(i).v2;
+//
+//         if (i + 1 == results.size() || conf != results.get(i + 1).v2) {
+//            rank[i] = i + 1;
+//         } else {
+//            int j = i + 1;
+//            for (; j < results.size() && conf == results.get(j).v2; j++) ;
+//            double r = (i + 1 + j) / 2.0;
+//            for (int k = i; k < j; k++) rank[k] = r;
+//            i = j - 1;
+//         }
+//      }
+//
+//      double auc = 0;
+//      for (int i = 0; i < results.size(); i++) {
+//         if (results.get(i).v1) {
+//            auc += rank[i];
+//         }
+//      }
+//
+//      auc = (auc - (positive * (positive + 1) / 2.0)) / (positive * negative);
+//      return auc;
    }
 
    @Override

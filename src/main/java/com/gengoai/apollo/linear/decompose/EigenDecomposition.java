ddba7e4dd683fcb6cf92ce99ddee5a6b4c7a3748
David Bracewell <david@davidbracewell.com>
2019-03-17 15:34:01 -0500

    Api updates, javadoc, speed improvements.

--- src/main/java/com/gengoai/apollo/linear/decompose/EigenDecomposition.java
+++ src/main/java/com/gengoai/apollo/linear/decompose/EigenDecomposition.java
@@ -1,10 +1,8 @@
 package com.gengoai.apollo.linear.decompose;
 
-import com.gengoai.apollo.linear.DenseMatrix;
-import com.gengoai.apollo.linear.NDArray;
-import com.gengoai.apollo.linear.NDArrayFactory;
-import com.gengoai.apollo.linear.RealMatrixWrapper;
+import com.gengoai.apollo.linear.*;
 import org.jblas.ComplexDoubleMatrix;
+import org.jblas.DoubleMatrix;
 import org.jblas.Eigen;
 
 import java.io.Serializable;
@@ -20,19 +18,35 @@ public class EigenDecomposition implements Decomposition, Serializable {
 
    @Override
    public NDArray[] decompose(NDArray input) {
-      if (input instanceof DenseMatrix) {
-         ComplexDoubleMatrix[] r = Eigen.eigenvectors(input.toDoubleMatrix()[0]);
-         NDArray[] toReturn = new NDArray[r.length];
-         for (int i = 0; i < r.length; i++) {
-            toReturn[i] = new DenseMatrix(r[i].getReal());
+      NDArray[][] toReturn = new NDArray[2][input.shape().sliceLength];
+      for (int i = 0; i < input.shape().sliceLength; i++) {
+         if (input.isDense()) {
+            DoubleMatrix slice = input.slice(i).toDoubleMatrix()[0];
+            ComplexDoubleMatrix[] result = Eigen.eigenvectors(slice);
+            for (int j = 0; j < result.length; j++) {
+               toReturn[j][i] = new DenseMatrix(result[j].getReal());
+            }
+         } else {
+            org.apache.commons.math3.linear.EigenDecomposition decomposition =
+               new org.apache.commons.math3.linear.EigenDecomposition(new RealMatrixWrapper(input.slice(i)));
+            toReturn[0][i] = NDArrayFactory.ND.array(decomposition.getV().getData());
+            toReturn[1][i] = NDArrayFactory.ND.array(decomposition.getD().getData());
          }
-         return toReturn;
       }
-      org.apache.commons.math3.linear.EigenDecomposition decomposition =
-         new org.apache.commons.math3.linear.EigenDecomposition(new RealMatrixWrapper(input));
-      return new NDArray[]{
-         NDArrayFactory.ND.array(decomposition.getV().getData()),
-         NDArrayFactory.ND.array(decomposition.getD().getData())
+      return new Tensor[]{
+         new Tensor(input.kernels(),
+                    input.channels(),
+                    toReturn[0]),
+         new Tensor(input.kernels(),
+                    input.channels(),
+                    toReturn[1])
       };
    }
+
+   public static void main(String[] args) throws Exception {
+      NDArray nd = NDArrayFactory.DENSE.rand(2, 2, 100, 100);
+      EigenDecomposition decomposition = new EigenDecomposition();
+      System.out.println(decomposition.decompose(nd)[0]);
+   }
+
 }// END OF EigenDecomposition
